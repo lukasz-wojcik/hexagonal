16e85640ee0a0ff5bcd2fb76fdc8da04d22aedc0
Łukasz Wójcik <lukasz.wojcik@simplybusiness.co.uk>
2021-07-22 00:03:51 +0200

    update fake payment service accepted args

--- lib/adapters/fake_payment_service.rb
+++ lib/adapters/fake_payment_service.rb
@@ -2,7 +2,7 @@ module Adapters
   class FakePaymentService
     Result = Struct.new(:'success?', keyword_init: true)
 
-    def process_payment(_payment_data)
+    def process_payment(amount:, payment_data:)
       Result.new(success?: true)
     end
 
