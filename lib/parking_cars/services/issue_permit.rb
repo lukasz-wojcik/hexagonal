e5365750cec1467664a02dce38e2e228285c6072
Łukasz Wójcik <lukasz.wojcik@simplybusiness.co.uk>
2021-07-26 17:21:09 +0200

    cleanup issue permit service

--- lib/parking_cars/services/issue_permit.rb
+++ lib/parking_cars/services/issue_permit.rb
@@ -12,28 +12,37 @@ module ParkingCars
       end
 
       def issue_permit(permit_request)
-        rate_name = permit_request.fetch(:rate_name)
+        rate_name, duration, payment_data, plate_number = *permit_request.slice(:rate_name,
+                                                                                :duration,
+                                                                                :payment_data,
+                                                                                :plate_number).values
         rate = rates_service.rate_by_name(rate_name)
-        duration = permit_request.fetch(:duration)
         raise "Cannot park in #{rate_name} for so long" unless rate.allowed_duration?(duration)
 
-        price = rate.price_for_duration(duration)
-        payment_processing_result = payment_service.process_payment(amount: price,
-                                                                    payment_data: permit_request.fetch(:payment_data))
-        if payment_processing_result.success?
-          repository.create(
-            {
-              code: SecureRandom.uuid,
-              rate_name: rate_name,
-              plate_number: permit_request.fetch(:plate_number),
-              start_date: DateTime.now,
-              end_date: DateTime.now + duration.minutes
-            }
-          )
-        end
+        payment_processing_result = collect_payment(duration, payment_data, rate)
+
+        return unless payment_processing_result.success?
+
+        crate_permit_record(duration, plate_number, rate_name)
       end
 
       private
+
+      def collect_payment(duration, payment_data, rate)
+        payment_service.process_payment(amount: rate.price_for_duration(duration), payment_data: payment_data)
+      end
+
+      def crate_permit_record(duration, plate_number, rate_name)
+        repository.create(
+          {
+            code: SecureRandom.uuid,
+            rate_name: rate_name,
+            plate_number: plate_number,
+            start_date: DateTime.now,
+            end_date: DateTime.now + duration.minutes
+          }
+        )
+      end
 
       attr_accessor :repository, :payment_service, :rates_service
     end
